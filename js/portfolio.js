32f45529d90729b5e5beeccf9009d596dcc800ac
Siddhi Shewale <sidshewale6978@gmail.com>
2022-09-26 11:57:10 +0530

    Add existing file

---
+++ js/portfolio.js
@@ -0,0 +1,24 @@
+// Navbar scroll
+
+function scrollprojects() {
+    window.scrollTo(0,730 );
+  }
+  
+  function scrollabout() {
+    window.scrollTo(0,1350);
+  }
+  
+  function scrollcontact() {
+    window.scrollTo(0, 2000);
+  }
+
+function changeMode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+ }
+
+ function changeModebulb() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
+
